45cfca3dddcdbe81a655a7b7a015be0f27a091f4
Filipp Zhuravlev <shiningfinger@list.ru>
2017-06-25 12:53:09 +0300

    Customed project. I'm going to layout it.

---
+++ src/blocks/components/Logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Logo = () => (
+  <div className='brand'>
+    <a id='brand' 
+       className='brand__refer' 
+       href="#">
+      <figure className='brand__name'></figure>
+    </a>
+  </div>
+);
+
+export default Logo;
